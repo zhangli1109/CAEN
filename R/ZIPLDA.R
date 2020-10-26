4e3077e9f975d01d0f59320b1ca261cf1681bd9f
FocusPaka <2160090406@emial.szu.edu.cn>
2020-10-26 10:33:37 +0800

    Update the ZIPLDA.R function.

--- R/ZIPLDA.R
+++ R/ZIPLDA.R
@@ -55,83 +55,95 @@
 #' class k. When rho is large then many of the elemtns of this matrix
 #' are shrunken towards 1(no over- or under-expression)."alpha" represents
 #' Power transformation used (if transform=TRUE).
-#'@examples
-#'dat <- newCountDataSet(n=40,p=500,sdsignal=0.1,K=4,param=10,drate=0.4)
-#'cv.out <- ZIPDA.cv(dat$x,dat$y)
-#'out <- ZIPLDA(dat$x,dat$y,dat$xte,rho=cv.out$bestrho)
+#' @examples
+#' library(SummarizedExperiment)
+#' dat <- newCountDataSet(n=40,p=500, K=4, param=10, sdsignal=0.1,drate=0.4)
+#' x <- t(assay(dat$sim_train_data))
+#' y <- as.numeric(colnames(dat$sim_train_data))
+#' xte <- t(assay(dat$sim_test_data))
+#' prob<-estimatep(x=x, y=y, xte=x, beta=1, type="mle", prior=NULL)
+#' prob0<-estimatep(x=x, y=y, xte=xte, beta=1,type="mle", prior=NULL)
+#' cv.out <- ZIPDA.cv(x=x, y=y, prob0=t(prob))
+#' out <- ZIPLDA(x=x, y=y, xte=xte, rho=cv.out$bestrho, prob0=t(prob0))
 #' @export
 
-ZIPLDA<-
-    function(x,y,xte=NULL,rho=0,beta=1,rhos=NULL,prob0=NULL,
-        type=c("mle","deseq","quantile"),
-        prior=NULL, transform=TRUE, alpha=NULL){
-    if(is.null(xte)){
-        xte <- x
-        warning("Since no xte was provided, testing was
+ZIPLDA <-
+    function(x, y, xte=NULL, rho=0, beta=1, rhos=NULL, prob0=NULL,
+             type=c("mle","deseq","quantile"),
+             prior=NULL, transform=TRUE, alpha=NULL){
+        if (is.null(xte)) {
+            xte <- x
+            warning("Since no xte was provided, testing was
             performed on training data set.")
-    }
-    if(!is.null(rho) && length(rho)>1)
-        stop("Can only enter 1 value of rho. If you would like
+        }
+        if (!is.null(rho) && length(rho) > 1)
+            stop("Can only enter 1 value of rho. If you would like
             to enter multiple values, use rhos argument.")
-    type <- match.arg(type)
-    if(!transform && !is.null(alpha)) stop("You have asked for
+        type <- match.arg(type)
+        if (!transform && !is.null(alpha)) stop("You have asked for
                 NO transformation but have entered alpha.")
-    if(transform && is.null(alpha)) alpha <- FindBestTransform(x)
-    if(transform){
-        if(alpha<=0 || alpha>1) stop("alpha must be between 0 and 1")
-        x <- x^alpha
-        xte <- xte^alpha
-    }
-    if(is.null(prior)) prior <- rep(1/length(unique(y)), length(unique(y)))
-    if(is.null(rho)&&is.null(rhos)) stop("Must enter rho or rhos.")
-    null.out <- NullModel(x, type=type)
-    ns <- null.out$n
-    nste <- NullModelTest(null.out,x,xte,type=type)$nste
-    uniq <- sort(unique(y))
-    signx<-sign(xte==0)
-    if(is.null(rhos)){
-        ds <- GetD(ns,x,y,rho,beta)
-        discriminant <- matrix(NA, nrow=nrow(xte), ncol=length(uniq))
-        for(k in seq(from = 1, to = length(uniq))){
-        for(i in seq(from = 1, to = nrow(xte))){
-            dstar = ds[k,]
-            part2=nste[i,]*dstar
-            part1=prob0[i,]+(1-prob0[i,])*exp(-part2)
-            part1[part1==0]=1
-            discriminant[i,k] <-sum(signx[i,]*log(part1))+
-                sum(xte[i,]*(1-signx[i,])*log(dstar))-sum((1-signx[i,])*part2)+
-                log(prior[k])
-
+        if (transform && is.null(alpha)) 
+            alpha <- PoiClaClu::FindBestTransform(x)
+        if (transform) {
+            if (alpha <= 0 || alpha > 1) stop("alpha must be between 0 and 1")
+            x <- x^alpha
+            xte <- xte^alpha
+        }
+        if (is.null(prior)) 
+            prior <- rep(1/length(unique(y)), length(unique(y)))
+        if (is.null(rho) && is.null(rhos)) stop("Must enter rho or rhos.")
+        null.out <- PoiClaClu::NullModel(x, type = type)
+        ns <- null.out$n
+        nste <- PoiClaClu::NullModelTest(null.out,x,xte,type = type)$nste
+        uniq <- sort(unique(y))
+        signx <- sign(xte == 0)
+        if (is.null(rhos)) {
+            ds <- GetD(ns,x,y,rho,beta)
+            discriminant <- matrix(NA, nrow = nrow(xte), ncol = length(uniq))
+            for (k in seq_len(length(uniq))) {
+                for (i in seq_len(nrow(xte))) {
+                    dstar = ds[k,]
+                    part2 = nste[i,]*dstar
+                    part1 = prob0[i,] + (1 - prob0[i,])*exp(-part2)
+                    part1[part1 == 0] = 1
+                    discriminant[i,k] <- sum(signx[i,]*log(part1)) +
+                        sum(xte[i,]*(1 - signx[i,])*log(dstar)) -
+                        sum((1 - signx[i,])*part2) + log(prior[k])
+                }
+            }
+            save <- list(ns = ns, nste = nste, ds = ds,
+                         discriminant = discriminant,
+                         ytehat = uniq[apply(discriminant,1,which.max)],
+                         alpha = alpha,rho = rho,x = x,y = y,xte = xte,
+                         type = type)
+            return(save)
+        } else {
+            save <- list()
+            ds.list <- GetD(ns,x,y,rho = NULL, rhos = rhos,beta)
+            for (rho in rhos) {
+                ds <- ds.list[[which(rhos == rho)]]
+                discriminant <- matrix(NA, nrow = nrow(xte), 
+                                       ncol = length(uniq))
+                for (k in seq_len(length(uniq))) {
+                    for (i in seq_len(nrow(xte))) {
+                        dstar = ds[k,]
+                        part2 = nste[i,]*dstar
+                        part1 = prob0[i] + (1 - prob0[i])*exp(-part2)
+                        part1[part1 == 0] = 1
+                        discriminant[i,k] <- sum(signx[i,]*log(part1)) +
+                            sum(xte[i,]*(1 - signx[i,])*log(dstar)) -
+                            sum((1 - signx[i,])*part2) + log(prior[k])
+                    }
+                }
+                save[[which(rhos == rho)]] <- 
+                    list(ns = ns,nste = nste,ds = ds, 
+                         discriminant = discriminant,
+                         ytehat = uniq[apply(discriminant,1,which.max)],
+                         alpha = alpha, rho = rho,x = x,y = y,
+                         xte = xte,type = type)
+            }
+            return(save)
         }
     }
-    save <- list(ns=ns,nste=nste,ds=ds,discriminant=discriminant,
-                ytehat=uniq[apply(discriminant,1,which.max)],
-                alpha=alpha,rho=rho,x=x,y=y,xte=xte,type=type)
-    return(save)
-    } else {
-        save <- list()
-        ds.list <- GetD(ns,x,y,rho=NULL, rhos=rhos,beta)
-        for(rho in rhos){
-        ds <- ds.list[[which(rhos==rho)]]
-        discriminant <- matrix(NA, nrow=nrow(xte), ncol=length(uniq))
-        for(k in seq(from = 1, to = length(uniq))){
-            for(i in seq(from = 1, to = nrow(xte))){
-            dstar = ds[k,]
-            part2=nste[i,]*dstar
-            part1=prob0[i]+(1-prob0[i])*exp(-part2)
-            part1[part1==0]=1
-            discriminant[i,k] <-sum(signx[i,]*log(part1))+sum(xte[i,]*
-                    (1-signx[i,])*log(dstar))-sum((1-signx[i,])*part2)
-                    +log(prior[k])
-        }
-        }
-        save[[which(rhos==rho)]] <- list(ns=ns,nste=nste,ds=ds,
-                discriminant=discriminant,
-                ytehat=uniq[apply(discriminant,1,which.max)],
-                alpha=alpha, rho=rho,x=x,y=y,xte=xte,type=type)
-        }
-        return(save)
-        }
-}
 
 
