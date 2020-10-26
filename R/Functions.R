bf0f6f4cbdf4af6c6e06fcf9d0775b6ec61bea7a
FocusPaka <2160090406@emial.szu.edu.cn>
2020-10-26 10:32:43 +0800

    Update the Functions.R function.

--- R/Functions.R
+++ R/Functions.R
@@ -1,31 +1,27 @@
 # 02/07/11 -- Helper functions.
 
 balanced.folds <- function(y, nfolds = min(min(table(y)), 10)){
-totals <- table(y)
+    totals <- table(y)
     fmax <- max(totals)
     nfolds <- min(nfolds, fmax)
-    # makes no sense to have more folds than the max class size
-    folds <- as.list(seq(nfolds))
     yids <- split(seq(y), y)
-    # nice way to get the ids in a list, split by class
-    ###Make a big matrix, with enough rows to get in all the folds per class
     bigmat <- matrix(NA, ceiling(fmax/nfolds) * nfolds, length(totals))
-    for(i in seq(totals)) {
-    bigmat[seq(totals[i]), i] <- sample(yids[[i]])
+    for (i in seq(totals)) {
+        bigmat[seq(totals[i]), i] <- sample(yids[[i]])
     }
-    smallmat <- matrix(bigmat, nrow = nfolds) # reshape the matrix
-    ### Now do a clever sort to mix up the NAs
-    smallmat <- permute.rows(t(smallmat)) ### Now a clever unlisting
+    smallmat <- matrix(bigmat, nrow = nfolds) 
+    smallmat <- permute.rows(t(smallmat)) 
     x <- apply(smallmat, 2, function(x) x[!is.na(x)])
-    if(is.matrix(x)){
-    xlist <- list()
-    for(i in seq(from = 1, to = ncol(x))){
-        xlist[[i]] <- x[,i]
-    }
-    return(xlist)
+    if (is.matrix(x)) {
+        xlist <- list()
+        for (i in seq(from = 1, to = ncol(x))) {
+            xlist[[i]] <- x[,i]
+        }
+        return(xlist)
     }
     return(x)
 }
+
 permute.rows <- function(x){
     dd <- dim(x)
     n <- dd[1]
@@ -36,44 +32,45 @@ permute.rows <- function(x){
 
 
 Soft <- function(x,a){
-    return(sign(x)*pmax(abs(x)-a,0))
+    return(sign(x)*pmax(abs(x) - a,0))
 }
 
 GetD <- function(ns, x, y, rho,beta,rhos=NULL){
-    if(!is.null(rho) && !is.null(rhos))
-    stop("do you want to use rho or rhos in GetD function???")
-    if(is.null(rhos)){
-    uniq <- sort(unique(y))
-    ds <- matrix(1, nrow=length(uniq), ncol=ncol(x))
-    for(k in seq(from = 1, to = length(uniq))){
-        a <- colSums(x[y==uniq[k],])+beta
-        b <- colSums(ns[y==uniq[k],])+beta
-        ds[k,] <- 1+Soft(a/b-1,rho/sqrt(b))
-    }
-    return(ds)
+    if (!is.null(rho) && !is.null(rhos))
+        stop("do you want to use rho or rhos in GetD function???")
+    if (is.null(rhos)) {
+        uniq <- sort(unique(y))
+        ds <- matrix(1, nrow = length(uniq), ncol = ncol(x))
+        for (k in seq(from = 1, to = length(uniq))) {
+            a <- colSums(x[y == uniq[k],]) + beta
+            b <- colSums(ns[y == uniq[k],]) + beta
+            ds[k,] <- 1 + Soft(a/b - 1,rho/sqrt(b))
+        }
+        return(ds)
     } else {
-    uniq <- sort(unique(y))
-    ds.list <- list()
-    for(rho in rhos){
-        ds <- matrix(1, nrow=length(uniq), ncol=ncol(x))
-        for(k in seq(from = 1, to = length(uniq))){
-        a <- colSums(x[y==uniq[k],])+beta
-        b <- colSums(ns[y==uniq[k],])+beta
-        ds[k,] <- 1+Soft(a/b-1,rho/sqrt(b))
-    }
-        ds.list[[which(rhos==rho)]] <- ds
-    }
+        uniq <- sort(unique(y))
+        ds.list <- list()
+        for (rho in rhos) {
+            ds <- matrix(1, nrow = length(uniq), ncol = ncol(x))
+            for (k in seq(from = 1, to = length(uniq))) {
+                a <- colSums(x[y == uniq[k],]) + beta
+                b <- colSums(ns[y == uniq[k],]) + beta
+                ds[k,] <- 1 + Soft(a/b - 1,rho/sqrt(b))
+            }
+        ds.list[[which(rhos == rho)]] <- ds
+        }
     return(ds.list)
     }
 }
 
+
 GetDn <- function(ns, x, y, beta){
     uniq <- sort(unique(y))
-    ds <- matrix(1, nrow=length(uniq), ncol=ncol(x))
-    for(k in seq(from = 1, to = length(uniq))){
-    a <- colSums(x[y==uniq[k],])+beta
-    b <- colSums(ns[y==uniq[k],])+beta
-    ds[k,] <- a/b
+    ds <- matrix(1, nrow = length(uniq), ncol = ncol(x))
+    for (k in seq(from = 1, to = length(uniq))) {
+        a <- colSums(x[y == uniq[k],]) + beta
+        b <- colSums(ns[y == uniq[k],]) + beta
+        ds[k,] <- a/b
     }
     return(ds)
 }
